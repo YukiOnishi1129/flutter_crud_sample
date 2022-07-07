12e168e94a2ca1555e4b192ed3888d3ba8af3e42
yuki <yuki.onishi1129@gmail.com>
2022-07-08 00:46:34 +0900

    view層作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_sample/view/item_list_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
